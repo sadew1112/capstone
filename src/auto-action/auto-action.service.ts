7e9963818bebbb8ee04316b7ba4136b88408d9d3
[sadew1112] <[bbg380027@gmail.com]>
2025-12-01 23:10:17 +0900

    avdfd

--- src/auto-action/auto-action.service.ts
+++ src/auto-action/auto-action.service.ts
@@ -211,7 +211,7 @@ export class AutoActionService {
   private async sendResultToAlertApi(
     result: SolutionExecutionResult,
   ): Promise<void> {
-    const url = `http://158.180.90.191:80/alerts/${result.alertId}/result`;
+    const url = `http://158.180.90.191:80/api/alerts/${result.alertId}/result`;
 
     try {
       await lastValueFrom(
